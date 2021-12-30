8a943745c7b90ed87d201ba773d7ba7cf4ba1577
Leisan Latypova <latley90@gmail.com>
2021-12-30 16:34:07 +0300

    Traffic Lights App

--- TrafficLights/ViewController.swift
+++ TrafficLights/ViewController.swift
@@ -7,13 +7,89 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-
+enum TrafficLightState {
+    case off
+    case red
+    case yellow
+    case green
 }
 
+class ViewController: UIViewController {
+
+    @IBOutlet private var redLightView: UIView!
+    @IBOutlet private var yellowLightView: UIView!
+    @IBOutlet private var greenLightView: UIView!
+    @IBOutlet private var startButton: UIButton!
+    
+    private var currentState = TrafficLightState.off
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        makeViewsRound()
+        showCurrentState()
+    }
+    
+    @IBAction private func startButtonPressed() {
+        switchCurrentStateToNextState()
+        showCurrentState()
+    }
+    
+    private func makeViewsRound() {
+        redLightView.layer.cornerRadius = 64
+        yellowLightView.layer.cornerRadius = 64
+        greenLightView.layer.cornerRadius = 64
+    }
+    
+    private func showCurrentState() {
+        
+        if currentState == .off {
+            redLightView.alpha = 0.3
+            yellowLightView.alpha = 0.3
+            greenLightView.alpha = 0.3
+            startButton.setTitle("START", for: .normal)
+        }
+        
+        if currentState == .red {
+            redLightView.alpha = 1
+            yellowLightView.alpha = 0.3
+            greenLightView.alpha = 0.3
+            startButton.setTitle("NEXT", for: .normal)
+            
+        }
+        
+        if currentState == .yellow {
+            redLightView.alpha = 0.3
+            yellowLightView.alpha = 1
+            greenLightView.alpha = 0.3
+            startButton.setTitle("NEXT", for: .normal)
+        }
+        
+        if currentState == .green {
+            redLightView.alpha = 0.3
+            yellowLightView.alpha = 0.3
+            greenLightView.alpha = 1
+            startButton.setTitle("NEXT", for: .normal)
+        }
+        
+    }
+
+    private func switchCurrentStateToNextState() {
+        
+        if currentState == .off {
+            currentState = .red
+        }
+        
+        else if currentState == .red {
+            currentState = .yellow
+        }
+        
+        else if currentState == .yellow {
+            currentState = .green
+        }
+        
+        else if currentState == .green {
+            currentState = .red
+        }
+    }
+}
